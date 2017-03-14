bdc6d9a1fcba5e89f9c0fdc869205e8056dc8d2c
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-03-14 10:55:31 -0500

    3/14/2017

---
+++ MobileInvoice.ios/NewClientController.cs
@@ -0,0 +1,61 @@
+using Foundation;
+using System;
+using UIKit;
+using AddressBookUI;
+
+namespace MobileInvoice.ios
+{
+    public partial class NewClientController : UITableViewController
+    {
+        public NewClientController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			this.TableView.AllowsSelection = false;
+
+			var tapContactGesture = new UITapGestureRecognizer(TapContact);
+			this.imgContact.UserInteractionEnabled = true;
+			this.imgContact.AddGestureRecognizer(tapContactGesture);
+
+			AddDoneButtonToKeyboard(txtName);
+			AddDoneButtonToKeyboard(txtPhone);
+		}
+
+		void TapContact()
+		{
+			ABPeoplePickerNavigationController contactController = new ABPeoplePickerNavigationController();
+			contactController.SelectPerson2 += ContactController_SelectPerson2;
+
+			PresentViewController(contactController, true, null);
+		}
+
+		void ContactController_SelectPerson2(object sender, ABPeoplePickerSelectPerson2EventArgs e)
+		{
+			
+		}
+
+		void AddDoneButtonToKeyboard(UITextField textField)
+		{
+			UIToolbar toolbar = new UIToolbar();
+
+			var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done);
+			doneButton.Clicked += delegate {
+				textField.ResignFirstResponder();
+			};
+
+			var bbs = new UIBarButtonItem[] {
+				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+				doneButton
+			};
+
+			toolbar.SetItems(bbs, false);
+			toolbar.SizeToFit();
+
+			textField.InputAccessoryView = toolbar;
+		}
+    }
+}
