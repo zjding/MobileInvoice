8df6020e09e477d7bd59adc777ed140ad9725a5b
zjding <zjding@gmail.com>
2017-03-21 20:41:08 -0500

    3/21/2017

--- MobileInvoice.db/dbo/Tables/Item.sql
+++ MobileInvoice.db/dbo/Tables/Item.sql
@@ -1,12 +1,11 @@
 ﻿CREATE TABLE [dbo].[Item] (
-    [Id]             BIGINT          IDENTITY (1, 1) NOT NULL,
-    [Name]           VARCHAR (100)   NULL,
-    [Price]          MONEY           NULL,
-    [Quantity]       INT             NULL,
-    [DiscountType]   VARCHAR (1)     NULL,
-    [DiscountAmount] DECIMAL (18, 2) NULL,
-    [Taxable]        BIT             NULL,
-    [Note]           VARCHAR (1000)  NULL,
-    CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Id]        BIGINT          NOT NULL,
+    [Name]      NVARCHAR (100)  NULL,
+    [UnitPrice] DECIMAL (19, 4) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
+
+
 
