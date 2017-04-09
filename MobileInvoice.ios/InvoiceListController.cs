a0cd6a6bc26cef298bc584fa529cf653d5ed3677
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-04-08 21:23:05 -0500

    4/8/2017

---
+++ MobileInvoice.ios/InvoiceListController.cs
@@ -0,0 +1,69 @@
+using Foundation;
+using System;
+using UIKit;
+using CoreGraphics;
+using System.Linq;
+
+namespace MobileInvoice.ios
+{
+    public partial class InvoiceListController : UITableViewController
+    {
+        public InvoiceListController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			SetupMenuView();
+
+
+		}
+
+		private void SetupMenuView()
+		{
+			var items = new string[] { "All", "Unpaid", "Paid", "Overdue" };
+
+			this.NavigationController.NavigationBar.Translucent = false;
+			this.NavigationController.NavigationBar.BarTintColor = new UIColor(
+				(nfloat)(55.0 / 255.0),
+				(nfloat)(187 / 255.0),
+				(nfloat)(153 / 255.0),
+				(nfloat)1.0
+			);
+
+			UINavigationBar.Appearance.TitleTextAttributes = new UIStringAttributes
+			{
+				ForegroundColor = UIColor.White
+			};
+
+			var menuView = new DropdownMenuView(
+				new CGRect(0.0, 0.0, 300, 44),
+				items.First(),
+				items,
+				this.View);
+
+			menuView.CellHeight = 50;
+			menuView.CellBackgroundColor = this.NavigationController.NavigationBar.TintColor;
+
+			menuView.CellSelectionColor = new UIColor(
+				(nfloat)(0.0 / 255.0),
+				(nfloat)(160.0 / 255.0),
+				(nfloat)(195.0 / 255.0),
+				(nfloat)1.0
+			);
+
+			menuView.CellTextLabelColor = UIColor.White;
+			menuView.CellTextLabelFont = UIFont.FromName(name: "AvenirNext-Medium", size: 16);
+			menuView.ArrowPadding = 15;
+			menuView.AnimationDuration = 0.3;
+			menuView.MaskBackgroundColor = UIColor.Black;
+			menuView.MaskBackgroundOpacity = (nfloat)0.3;
+			menuView.BounceOffset = 5;
+
+
+			this.NavigationItem.TitleView = menuView;
+		}
+    }
+}
