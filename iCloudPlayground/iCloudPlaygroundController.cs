e756d4d5ef47e0af39aa482eec53e157ee8886a9
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-04-19 15:22:50 -0500

    4/19/2017

--- iCloudPlayground/iCloudPlaygroundController.cs
+++ iCloudPlayground/iCloudPlaygroundController.cs
@@ -1,18 +1,40 @@
 using Foundation;
 using System;
 using UIKit;
+using CloudKit;
 
 namespace iCloudPlayground
 {
     public partial class iCloudPlaygroundController : UITableViewController
     {
-        public iCloudPlaygroundController (IntPtr handle) : base (handle)
+		private const string ReferenceItemRecordName = "ReferenceItems";
+
+		#region Computed Properties
+		public AppDelegate ThisApp
+		{
+			get { return (AppDelegate)UIApplication.SharedApplication.Delegate; }
+		}
+		#endregion
+
+		public iCloudPlaygroundController (IntPtr handle) : base (handle)
         {
         }
 
 		partial void btnSave_UpInside(UIButton sender)
 		{
-			throw new NotImplementedException();
+
+			// Create a new record
+			var newRecord = new CKRecord(ReferenceItemRecordName);
+			newRecord["FirstName"] = (NSString)"Jason";
+			newRecord["LastName"] = (NSString)"Ding";
+
+			// Save it to the database
+			ThisApp.PublicDatabase.SaveRecord(newRecord, (record, err) => {
+			    // Was there an error?
+			    if (err != null) {
+			    
+			    }
+			});
 		}
 	}
 }
