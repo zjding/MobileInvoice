a780c60a627cb9841219a3a417cab6fa493ccd40
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-04-19 14:29:52 -0500

    Add iCloudPlayground project

---
+++ iCloudPlayground/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using UIKit;
+using CloudKit;
+
+namespace iCloudPlayground
+{
+	public partial class ViewController : UIViewController
+	{
+		#region Computed Properties
+		public AppDelegate ThisApp
+		{
+			get { return (AppDelegate)UIApplication.SharedApplication.Delegate; }
+		}
+		#endregion
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
---
+++ iCloudPlayground/iCloudPlaygroundController.cs
@@ -0,0 +1,18 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace iCloudPlayground
+{
+    public partial class iCloudPlaygroundController : UITableViewController
+    {
+        public iCloudPlaygroundController (IntPtr handle) : base (handle)
+        {
+        }
+
+		partial void btnSave_UpInside(UIButton sender)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
