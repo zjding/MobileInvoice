a02bdd3def54059002b498bb738c0d6e28f54375
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-06-06 14:37:13 -0500

    a

---
+++ MobileInvoice.ios/InvoicePreviewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MobileInvoice.ios
+{
+    public partial class InvoicePreviewController : UIViewController
+    {
+        public InvoicePreviewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ MobileInvoice.ios/InvoicePreviewNavigationController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MobileInvoice.ios
+{
+    public partial class InvoicePreviewNavigationController : UINavigationController
+    {
+        public InvoicePreviewNavigationController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
